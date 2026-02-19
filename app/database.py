40bc849d623c2f64e7b64c783e52bbad01d286ab
Vansh Raipurkar <raipurkarvansh092@gmail.com>
2026-02-19 17:48:47 +0530

    updated database configuration for Render
    Updated DATABASE_URL to use environment variable.

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Raipurkarvs10%40@localhost/krishimitra_clean"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
